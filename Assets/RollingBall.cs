207b139b7df268f3de33db83aa4fc87af075a2a0
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-09-21 23:02:21 +0200

    Update RollingBall.cs

--- Assets/RollingBall.cs
+++ Assets/RollingBall.cs
@@ -9,7 +9,8 @@ public class RollingBall : MonoBehaviour
     #region hastighet og vector 3 for hastighet og posisjon
     [SerializeField] private Vector2 ballpos = Vector2.zero;
     [SerializeField]  private Vector3 deltaPos = Vector3.zero;
-    [SerializeField]  private Vector3 currentPos = Vector3.zero;
+    [SerializeField]  private Vector3 currentPosition = Vector3.zero;
+    [SerializeField]  private Vector3 previousPosition = Vector3.zero;
     [SerializeField] private Vector3 _prevPos;
     [SerializeField]  private Vector3 currentVelocity = Vector3.zero;
     [SerializeField] private Vector3 previousVelocity = Vector3.zero;
@@ -42,19 +43,85 @@ public class RollingBall : MonoBehaviour
     private float _mass = 0.004f;
 
 
+    [SerializeField] private Vector3 currentTriangle = Vector3.zero;
+    [SerializeField] private Vector3 previousTriangle = Vector3.zero;
+
+    [SerializeField] private Vector3 v0, v1, v2 = Vector3.zero;
+    
+    [SerializeField] private Vector3 accelerationVector = Vector3.zero;
 
     public void Move()
     {
+        // Finne t r e k a n t
+        for(var i = 0; i < myTrekant.mesh.triangles.Length; i+=3)
+        {
+            current_Index = i / 3;
+            int index_0 = myTrekant.mesh.triangles[i];
+            int index_1 = myTrekant.mesh.triangles[i+1];
+            int index_2 = myTrekant.mesh.triangles[i+2];
+            
+            // Finn trekantens vertices v0 , v1 , v2
+            v0 = new Vector3(myTrekant.mesh.vertices[index_0].x,myTrekant.mesh.vertices[index_0].y, myTrekant.mesh.vertices[index_0].z);
+            v1 = new Vector3(myTrekant.mesh.vertices[index_1].x,myTrekant.mesh.vertices[index_1].y, myTrekant.mesh.vertices[index_1].z);
+            v2 = new Vector3(myTrekant.mesh.vertices[index_2].x,myTrekant.mesh.vertices[index_2].y, myTrekant.mesh.vertices[index_2].z);
+
+            // Finn ballensposisjon i xy=planet
+            // Soek etter triangel som ballen er pa na
+            // med barysentriske koordinater
+            barysentricCoordinateToBall = Barycentric(new Vector2(v0.x, v0.z), new Vector2(v1.x, v1.z),
+                new Vector2(v2.x, v2.z), new Vector2(transform.position.x, transform.position.z));
+            
+            if (barysentricCoordinateToBall is {x: >= 0, y: >= 0, z: >= 0})
+            {
+                // beregne normal
+                currentNormal = Vector3.Cross(v1 - v0, v2 - v0);
+                
+                // beregn akselerasjons vektor = ligning 8.12
+                accelerationVector = Physics.gravity.y * new Vector3(
+                    currentNormal.x * currentNormal.z,
+                    currentNormal.y * currentNormal.y - 1f,
+                    currentNormal.z * currentNormal.y
+                );
+
+                currentVelocity = previousVelocity + accelerationVector * Time.fixedDeltaTime;
+
+                currentPosition = previousPosition + previousVelocity * Time.fixedDeltaTime;
+               
+                
+                
+                // Oppdaterer hastighet og posisjon
+                // ligning ( 8 . 1 4 ) og ( 8 . 1 5 )
+                
+                if (current_Index != previous_Index)
+                {
+                // Ballen har rullet over pa nytt triangel
+                // Beregner normalen til kollisjonsplanet,
+                // se ligning ( 8 . 1 7 )
+                // Korrigere posisjon oppover i normalens retning
+                // Oppdater hastighetsvektoren , se ligning ( 8 . 1 6 )
+                // Oppdatere posisjon i retning den nye
+                // hastighets vektoren
+                }
+                
+            // Oppdater gammel normal og indeks
+            previousNormal = currentNormal;
+            previous_Index = current_Index;
+            }
+        }
+        
         
     }
     
     
+// transform.position += new Vector3(0f, 0.01f, 0f);
     
 
     private void FixedUpdate()
     {
         
+ 
         
+        Move();
         
         
         
