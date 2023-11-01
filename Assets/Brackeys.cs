197a35b034b7560d4346ce73aca33ffe290b212a
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-11-01 16:32:24 +0100

    tutorial follow

--- Assets/Brackeys.cs
+++ Assets/Brackeys.cs
@@ -9,9 +9,9 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class Brackeys : MonoBehaviour
 {
+
+
     private Mesh mesh;
-
-
     private Vector3[] vertices; 
     private int[] triangles;
     
@@ -31,27 +31,46 @@ public class Brackeys : MonoBehaviour
     void CreateShape()
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-
+        
+        
         for (int z = 0, i = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
                 // looping all the vertices
                 // need an index
-                vertices[i] = new Vector3(x, 0, z);
+
+                float y = Mathf.PerlinNoise(x * .3f, z*.3f) * 2f;
+                
+                vertices[i] = new Vector3(x, y, z);
                 i++;
 
             }
         }
 
-        triangles = new int[6];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
-        triangles[3] = 1;
-        triangles[4] = xSize + 1;
-        triangles[5] = xSize + 2;
-//
+        int vert = 0;
+        int tris = 0;
+        triangles = new int[xSize*zSize*6];
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+            
+                vert++;
+                tris += 6;
+            } 
+            vert++;
+
+        }
+        
+        
 
 
     }
